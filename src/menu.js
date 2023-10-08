d36ce76bf9866b3e98efe7a5a06c1862939331c0
JHE4N <93744970+JHE4N@users.noreply.github.com>
2023-10-08 15:28:00 -0400

    criado o menu

---
+++ src/menu.js
@@ -0,0 +1,18 @@
+const iconeBars = document.getElementById('iconBars');
+const iconeX = document.getElementById('iconX');
+const menu = document.getElementById('itens-menu')
+
+
+iconeBars.addEventListener('click', function(){
+    iconeBars.style.display = 'none';
+    iconeX.style.display = 'block';
+
+    menu.style.right = '0';
+});
+
+iconeX.addEventListener('click', function(){
+    iconeBars.style.display = 'block';
+    iconeX.style.display = 'none';
+    menu.style.right = '-900px';
+});
+
