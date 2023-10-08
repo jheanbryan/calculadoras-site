d379a5938e3c057a7fe41a81e8621df280ea2235
JHE4N <93744970+JHE4N@users.noreply.github.com>
2023-10-08 12:22:40 -0400

    realizando o calculo da TMB

--- src/tmb.js
+++ src/tmb.js
@@ -1,18 +1,20 @@
-const peso = document.getElementById('peso');
-const altura = document.getElementById('altura');
-const idade = document.getElementById('idade');
 let genero;
-
 const btn = document.getElementById('btn-calcular');
-
+const resultado = document.getElementById('resultado');
 btn.addEventListener('click',function(e){
     e.preventDefault();
 
+    let peso = Number(document.getElementById('peso').value);
+    let altura = Number(document.getElementById('altura').value);
+    let idade = Number(document.getElementById('idade').value);
+
     if(document.getElementById('masculino').checked){
-        console.log('Masculino')
-        genero = 'masculino';
+        let tmbHomem = (66 + 13.7 * peso + 5 * altura - 6.8 * idade).toFixed(2);
+        console.log(tmbHomem);
+        resultado.innerText = `${tmbHomem}Kcal`
     } else if(document.getElementById('feminino').checked){
-        console.log('feminino')
-        genero = 'feminino';
+        let tmbMulher = (665 + 9.6 * peso + 1.8 * altura - 4.7 * idade).toFixed(2);
+        console.log(tmbMulher);
+        resultado.innerText = `${tmbMulher}Kcal`
     }
 })
